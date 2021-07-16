75969478eeef58402670222acc57a96fada14783
K-C-Martin <57388565+K-C-Martin@users.noreply.github.com>
2021-07-16 03:05:33 +0200

    Calculate MAE and RMSE for all psets

--- 4 - r3PG predictions compare.R
+++ 4 - r3PG predictions compare.R
@@ -16,8 +16,10 @@ r3PG_output <- read.csv(ofilename)
 
 ## CREATE DATAFRAMES TO HOLD CALCULATED MAE AND RMSE VALUES
 
-MAE_df <- RMSE_df <- data.frame(cbind(paste0('pset', 1:num_par_comb)), 
-                                matrix(NA, ncol = length(oput) + 1, nrow = num_par_comb))
+all_psets <- paste0('pset', 1:num_par_comb)
+
+MAE_df <- RMSE_df <- data.frame(cbind(all_psets, 
+                                matrix(NA, ncol = length(oput) + 1, nrow = num_par_comb)))
 
 colnames(MAE_df) <-  c('parameter set', paste(oput, 'MAE'), 'combined MAE')
 
@@ -27,12 +29,63 @@ colnames(RMSE_df) <-  c('parameter set', paste(oput, 'RMSE'), 'combined RMSE')
 
 # CALCULATE MAE AND RMSE FOR EACH OUTPUT VARIABLE FOR EACH PARAMETER SET
 
-for (pset in 1:num_par_comb) {
+for (o in oput) {
   
   
+  o_MAE <- o_RMSE <- numeric(length = num_par_comb)
   
+  
+  for (pset in 1:num_par_comb) {
+    
+    sub_output <- r3PG_output[(r3PG_output[,1] == paste0('pset', pset)), ]
+    
+    
+    actual <- sub_output[(sub_output$variable == o), ]$actual
+    
+    predicted <- sub_output[(sub_output$variable == o), ]$predicted
+    
+    
+    o_MAE[pset] <- calc_MAE(actual, predicted)
+    
+    o_RMSE[pset] <- calc_RMSE(actual, predicted)
+    
+  }
+  
+  
+  MAE_df[, match(paste(o, 'MAE'), colnames(MAE_df))] <- o_MAE
+  
+  RMSE_df[, match(paste(o, 'RMSE'), colnames(RMSE_df))] <- o_RMSE
   
 }
 
 
-ofilename <- paste0('output compare/', tstamp, '_par_test_r3PG_MAE.csv')
+
+for (pset in 1:num_par_comb) {
+  
+  sub_output <- r3PG_output[(r3PG_output[,1] == paste0('pset', pset)), ]
+  
+  
+  actual <- sub_output$actual
+  
+  predicted <- sub_output$predicted
+  
+  
+  o_MAE[pset] <- calc_MAE(actual, predicted)
+  
+  o_RMSE[pset] <- calc_RMSE(actual, predicted)
+  
+}
+
+MAE_df[, match('combined MAE', colnames(MAE_df))] <- o_MAE
+
+RMSE_df[, match('combined RMSE', colnames(RMSE_df))] <- o_RMSE
+
+
+
+
+write.csv(MAE_df, file = paste0('output compare/', tstamp, '_par_test_r3PG_MAE.csv'), row.names=F)
+
+write.csv(RMSE_df, file = paste0('output compare/', tstamp, '_par_test_r3PG_RMSE.csv'), row.names=F)
+          
+          
+                    
